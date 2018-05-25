0b42c193adfd5454c846c1c4525006948c8ba2f1
kondrat1997 <vladimir.kondratenko@phystech.edu>
2018-05-26 00:09:44 +0300

    added proxy to trick RKN

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import config
 import telebot
 import time
+import os
+from os.path import join as jp
+from config import *
 
 bot = telebot.TeleBot(config.token)
 
@@ -10,9 +13,12 @@ def simple_echo(message):
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
+    bot.PATH = os.path.dirname(__file__)
     while True:
         try:
             bot.polling(none_stop=True)
 
         except Exception as e:
+            print(f'EXCEPTION: {e}')
             time.sleep(5)
+            break
--- config.py
+++ config.py
@@ -1,2 +1,8 @@
+from telebot import apihelper
+
 # you should define your bot-token here
 token = ''
+
+# proxy
+# h means remote DNS, because faggots from RKN banned it
+apihelper.proxy = {'http': 'socks5h://localhost:1080', 'https': 'socks5h://localhost:1080'}
