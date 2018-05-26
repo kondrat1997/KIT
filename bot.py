319d57fbd7f7934b7fd4770b59054559bf36ef7d
elenaermakova <lena0707es@gmail.com>
2018-05-26 03:00:53 +0300

    added halp.jpg

--- bot.py
+++ bot.py
@@ -1,14 +1,28 @@
 import time
 import os
 from os.path import join as jp
+import subprocess
 from commands import *
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    with open(jp(bot.PATH,'img/help.jpg'), 'rb') as pic:
+        bot.send_photo(message.chat.id, pic)
+
+    ans = 'Вот что я умею:\n'
+    for cmd in CMD:
+        if cmd not in hCMD:
+            ans = ans + cmd + '\n'
+
+    bot.send_message(message.chat.id, ans)
+commands['/help'] = help
 
 if __name__ == '__main__':
     bot.PATH = os.path.dirname(__file__)
+    print(bot.PATH)
     while True:
         try:
             bot.polling(none_stop=True)
-
         except Exception as e:
             print(f'EXCEPTION: {e}')
             time.sleep(5)
--- commands.py
+++ commands.py
@@ -14,17 +14,6 @@ def start(message):
 commands['/start'] = start
 
 
-@bot.message_handler(commands=['help'])
-def help(message):
-    ans = 'Вот что я умею:\n'
-    for cmd in CMD:
-        if cmd not in hCMD:
-            ans = ans + cmd + '\n'
-
-    bot.send_message(message.chat.id, ans)
-commands['/help'] = help
-
-
 @bot.message_handler(commands=['hhelp'])
 def hhelp(message):
     ans = 'Скрытый help. Вот что я умею:\n'
@@ -40,7 +29,7 @@ hCMD.append('/hhelp')
 def kill(message):
     ans = 'Оййй'
     bot.send_message(message.chat.id, ans)
-    os.system("kill -9 `ps -ef | grep python\ bot| grep -v grep | awk '{print $2}'`")
+    os.system("kill -9 `ps -ef | grep python\ *bot| grep -v grep | awk '{print $2}'`")
 
 commands['/kill'] = kill
 hCMD.append('/kill')
