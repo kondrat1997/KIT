c2e802d1196dc09847186c331bf42359842eac4d
kondrat1997 <vladimir.kondratenko@phystech.edu>
2018-05-30 16:50:59 +0300

    added scripts and commands for OrangePi

--- scripts/pull_kit.sh
+++ scripts/pull_kit.sh
@@ -2,5 +2,5 @@
 kill -9 `ps -ef | grep /root/py3/bin/python\ /tmp/KIT/bot.py| grep -v grep | awk '{print $2}'`
 rm -rf /tmp/KIT
 git -C /root/KIT pull
-cp -r /root/KIT /tmp/KIT
+. /root/copy_bot_and_token.sh
 /tmp/KIT/bot.py
