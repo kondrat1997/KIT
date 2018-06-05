aa67847a5e0877e5959fe8996c6734b88aef679d
kondrat1997 <vladimir.kondratenko@phystech.edu>
2018-06-05 03:08:27 +0300

    pull_kit.sh has been changed

--- scripts/pull_kit.sh
+++ scripts/pull_kit.sh
@@ -2,5 +2,4 @@
 kill -9 `ps -ef | grep /root/py3/bin/python\ /tmp/KIT/bot.py| grep -v grep | awk '{print $2}'`
 rm -rf /tmp/KIT
 git -C /root/KIT pull
-. /root/copy_bot_and_token.sh
-/tmp/KIT/bot.py
+. /root/KIT_scripts/start_KIT.sh
